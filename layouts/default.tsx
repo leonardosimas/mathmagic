be20a45cab8236fd54b5df269bdf31a7287525de
Leonardo Simas Rodrigues da Silva <leonardosimasrodrigues@gmail.com>
2023-08-18 19:41:40 -0300

    Refeita lógica e adicionado 4 jogos distintos

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -12,17 +12,17 @@ export default function DefaultLayout({
 		<div className="relative flex flex-col h-screen bg-zinc-400">
 			<Head />
 			<Navbar />
-			<main className="container mx-auto max-w-7xl px-6 flex-grow">
+			<main className="container mx-auto max-w-7xl px-6 flex-grow bg-zinc-400">
 				{children}
 			</main>
-			<footer className="w-full flex items-center justify-center py-3">
+			<footer className="w-full flex items-center justify-center py-3 bg-zinc-400">
 				<Link
 					isExternal
 					className="flex items-center gap-1 text-current"
 					href="https://github.com/leonardosimas"
 					title="Github Leonardo Simas"
 				>
-					<p className="text-primary">Criado por Leonardo Simas</p>
+					<p className="text-primary">Criado por Leonardo Simas versão alfa 0.01A</p>
     				<div className="text-default-600">© {currentYear}</div>
 				</Link>
 			</footer>
